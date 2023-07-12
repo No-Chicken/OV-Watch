b0ada7a48a6c200d8c15f6eeb875c877c20fa7b7
Kingham <1425962791@qq.com>
2023-07-12 16:05:43 +0800

    currect the iic bus
    currect the EEPROM(BL24C02)'s SDA&SCL pin in /Hardware/BL24C02/BL24C02.c

--- Software/OV-Watch-V2.2/Hardwares/BL24C02/BL24C02.c
+++ Software/OV-Watch-V2.2/Hardwares/BL24C02/BL24C02.c
@@ -6,8 +6,8 @@ iic_bus_t BL_bus =
 {
 	.IIC_SDA_PORT = GPIOA,
 	.IIC_SCL_PORT = GPIOA,
-	.IIC_SDA_PIN  = GPIO_PIN_6,
-	.IIC_SCL_PIN  = GPIO_PIN_7,
+	.IIC_SDA_PIN  = GPIO_PIN_11,
+	.IIC_SCL_PIN  = GPIO_PIN_12,
 };
 
 void BL24C02_Write(uint8_t addr,uint8_t length,uint8_t buff[])
