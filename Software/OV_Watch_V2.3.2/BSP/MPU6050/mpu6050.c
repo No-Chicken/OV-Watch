71d5b59fe01a2652af038f5ef9189a26b451ce42
Kingham <1425962791@qq.com>
2023-08-08 11:49:54 +0800

    add some annotation

--- Software/OV_Watch_V2.3.2/BSP/MPU6050/mpu6050.c
+++ Software/OV_Watch_V2.3.2/BSP/MPU6050/mpu6050.c
@@ -13,6 +13,14 @@ iic_bus_t MPU_bus =
 	.IIC_SCL_PIN  = GPIO_PIN_14,
 };
 
+
+/**************************************************************************/
+/*!
+    @brief  initialize the iic port connect with MPU6050
+
+    @param  NULL
+*/
+/**************************************************************************/
 void MPU_INT_Pin_Init()
 {
 	GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -31,6 +39,14 @@ void MPU_INT_Pin_Init()
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 }
 
+
+/**************************************************************************/
+/*!
+    @brief  initialize the motion function of MPU6050
+
+    @param  NULL
+*/
+/**************************************************************************/
 void MPU_Motion_Init(void)			
 {
     MPU_Write_Byte(MPU_MOTION_DET_REG,0x01);    //set the acceleration threshold is (LSB*2)mg
@@ -39,30 +55,45 @@ void MPU_Motion_Init(void)
     MPU_Write_Byte(MPU_INT_EN_REG,0x40);       	//enable INT
 }
 
+
+/**************************************************************************/
+/*!
+    @brief  initialize the IIC bus
+
+    @param  NULL
+*/
+/**************************************************************************/
 void MPU_Bus_Init(void)
 {
 	CLK_ENABLE;
 	IICInit(&MPU_bus);
 }
 
-//��ʼ��MPU6050
-//����ֵ:0,�ɹ�
-//    ����,�������
+
+/**************************************************************************/
+/*!
+    @brief  init the MPU6050
+
+    @param  NULL
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Init(void)
 { 
 	u8 res;
 	
 	MPU_Bus_Init();
 	
-	MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X80);	//��λMPU6050
+	MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X80);	//复位MPU6050
   delay_ms(100);
-	MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X00);	//����MPU6050 
-	MPU_Set_Gyro_Fsr(3);										//�����Ǵ�����,��2000dps
-	MPU_Set_Accel_Fsr(2);										//���ٶȴ�����,��8g
-	MPU_Set_Rate(50);												//���ò�����50Hz
-	MPU_Write_Byte(MPU_INT_EN_REG,0X00);		//�ر������ж�
-	MPU_Write_Byte(MPU_USER_CTRL_REG,0X00);	//IIC��ģʽ�ر�
-	MPU_Write_Byte(MPU_FIFO_EN_REG,0X00);		//�ر�FIFO
+	MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X00);	//唤醒MPU6050 
+	MPU_Set_Gyro_Fsr(3);										//G传感器, 2000dps
+	MPU_Set_Accel_Fsr(2);										//A传感器, 8g
+	MPU_Set_Rate(50);												//采样率50Hz
+	MPU_Write_Byte(MPU_INT_EN_REG,0X00);		//关闭所有中断
+	MPU_Write_Byte(MPU_USER_CTRL_REG,0X00);	//IIC主模式关闭
+	MPU_Write_Byte(MPU_FIFO_EN_REG,0X00);		//dis FIFO
 	MPU_Write_Byte(MPU_INTBP_CFG_REG,0X80);	//INT active low
 	
 	res=MPU_Read_Byte(MPU_DEVICE_ID_REG);
@@ -70,7 +101,7 @@ u8 MPU_Init(void)
 	{
 		MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X28);	//SET the internal 8MHz,sleep=0,cycle=1,TEMP_DIS=1//low power modes
 		MPU_Write_Byte(MPU_PWR_MGMT2_REG,0X87);	//enable accelerometer,disanable gyroscope,set the wake up frequence=20Hz
-		MPU_Set_Rate(50);												//���ò�����Ϊ50Hz
+		MPU_Set_Rate(50);												//采样率50Hz
  	}else return 1;
 	
 	MPU_Motion_Init();
@@ -96,26 +127,46 @@ uint8_t MPU_Read_Status()
 	return MPU_Read_Byte(MPU_INT_STA_REG);
 }
 
-//����MPU6050�����Ǵ����������̷�Χ
-//fsr:0,��250dps;1,��500dps;2,��1000dps;3,��2000dps
-//����ֵ:0,���óɹ�
-//    ����,����ʧ�� 
+
+/**************************************************************************/
+/*
+    @brief  设置MPU6050陀螺仪传感器满量程范围
+
+    @param  fsr:0,+250dps;1,500dps;2,+1000dps;3,+2000dps
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Set_Gyro_Fsr(u8 fsr)
 {
-	return MPU_Write_Byte(MPU_GYRO_CFG_REG,fsr<<3);//���������������̷�Χ  
+	return MPU_Write_Byte(MPU_GYRO_CFG_REG,fsr<<3); 
 }
-//����MPU6050���ٶȴ����������̷�Χ
-//fsr:0,��2g;1,��4g;2,��8g;3,��16g
-//����ֵ:0,���óɹ�
-//    ����,����ʧ�� 
+
+
+/**************************************************************************/
+/*
+    @brief  设置MPU6050的数字低通滤波器
+
+    @param  fsr:低通滤波器频率(Hz)
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Set_Accel_Fsr(u8 fsr)
 {
-	return MPU_Write_Byte(MPU_ACCEL_CFG_REG,fsr<<3);//���ü��ٶȴ����������̷�Χ  
+	return MPU_Write_Byte(MPU_ACCEL_CFG_REG,fsr<<3);
 }
-//����MPU6050�����ֵ�ͨ�˲���
-//lpf:���ֵ�ͨ�˲�Ƶ��(Hz)
-//����ֵ:0,���óɹ�
-//    ����,����ʧ�� 
+
+
+/**************************************************************************/
+/*
+    @brief  设置MPU6050的低通滤波器
+
+    @param  lpf: Hz
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Set_LPF(u16 lpf)
 {
 	u8 data=0;
@@ -125,24 +176,38 @@ u8 MPU_Set_LPF(u16 lpf)
 	else if(lpf>=20)data=4;
 	else if(lpf>=10)data=5;
 	else data=6; 
-	return MPU_Write_Byte(MPU_CFG_REG,data);//�������ֵ�ͨ�˲���  
+	return MPU_Write_Byte(MPU_CFG_REG,data);
 }
-//����MPU6050�Ĳ�����(�ٶ�Fs=1KHz)
-//rate:4~1000(Hz)
-//����ֵ:0,���óɹ�
-//    ����,����ʧ�� 
+
+
+/**************************************************************************/
+/*
+    @brief  设置MPU6050的采样率
+
+    @param  rate: 4~1000 Hz
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Set_Rate(u16 rate)
 {
 	u8 data;
 	if(rate>1000)rate=1000;
 	if(rate<4)rate=4;
 	data=1000/rate-1;
-	data=MPU_Write_Byte(MPU_SAMPLE_RATE_REG,data);	//�������ֵ�ͨ�˲���
- 	return MPU_Set_LPF(rate/2);	//�Զ�����LPFΪ�����ʵ�һ��
-}
+	data=MPU_Write_Byte(MPU_SAMPLE_RATE_REG,data);
+ 	return MPU_Set_LPF(rate/2);
 
-//�õ��¶�ֵ
-//����ֵ:�¶�ֵ(������100��)
+
+/**************************************************************************/
+/*
+    @brief  获取MPU6050温度值
+
+    @param  NULL
+
+    @return temperature (short)
+*/
+/**************************************************************************/
 short MPU_Get_Temperature(void)
 {
     u8 buf[2]; 
@@ -153,10 +218,17 @@ short MPU_Get_Temperature(void)
     temp=36.53+((double)raw)/340;  
     return temp*100;;
 }
-//�õ�������ֵ(ԭʼֵ)
-//gx,gy,gz:������x,y,z���ԭʼ����(������)
-//����ֵ:0,�ɹ�
-//    ����,�������
+
+
+/**************************************************************************/
+/*
+    @brief  获取MPU6050陀螺仪原始值
+
+    @param  NULL
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Get_Gyroscope(short *gx,short *gy,short *gz)
 {
     u8 buf[6],res;  
@@ -169,10 +241,17 @@ u8 MPU_Get_Gyroscope(short *gx,short *gy,short *gz)
 		} 	
     return res;;
 }
-//�õ����ٶ�ֵ(ԭʼֵ)
-//gx,gy,gz:������x,y,z���ԭʼ����(������)
-//����ֵ:0,�ɹ�
-//    ����,�������
+
+
+/**************************************************************************/
+/*
+    @brief  获取MPU6050加速度原始值
+
+    @param  NULL
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Get_Accelerometer(short *ax,short *ay,short *az)
 {
     u8 buf[6],res;  
@@ -185,29 +264,35 @@ u8 MPU_Get_Accelerometer(short *ax,short *ay,short *az)
 		} 	
     return res;;
 }
-//IIC����д
-//addr:������ַ 
-//reg:�Ĵ�����ַ
-//len:д�볤��
-//buf:������
-//����ֵ:0,����
-//    ����,�������
+
+/**************************************************************************/
+/*
+    @brief  IIC连续写
+
+    @param  addr:器件地址
+    @param  reg:寄存器地址
+	@param  len:写入长度
+	@param  buf:数据区
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Write_Len(u8 addr,u8 reg,u8 len,u8 *buf)
 {
 	u8 i; 
   IICStart(&MPU_bus); 
-	IICSendByte(&MPU_bus,(addr<<1)|0);//����������ַ+д����	
-	if(IICWaitAck(&MPU_bus))	//�ȴ�Ӧ��
+	IICSendByte(&MPU_bus,(addr<<1)|0);
+	if(IICWaitAck(&MPU_bus))	
 	{
 		IICStop(&MPU_bus);		 
 		return 1;		
 	}
-    IICSendByte(&MPU_bus,reg);	//д�Ĵ�����ַ
-    IICWaitAck(&MPU_bus);		//�ȴ�Ӧ��
+    IICSendByte(&MPU_bus,reg);	
+    IICWaitAck(&MPU_bus);		
 	for(i=0;i<len;i++)
 	{
-		IICSendByte(&MPU_bus,buf[i]);	//��������
-		if(IICWaitAck(&MPU_bus))		//�ȴ�ACK
+		IICSendByte(&MPU_bus,buf[i]);	
+		if(IICWaitAck(&MPU_bus))		
 		{
 			IICStop(&MPU_bus);	 
 			return 1;		 
@@ -217,24 +302,29 @@ u8 MPU_Write_Len(u8 addr,u8 reg,u8 len,u8 *buf)
 	return 0;	
 } 
 
-//IICдһ���ֽ� 
-//reg:�Ĵ�����ַ
-//data:����
-//����ֵ:0,����
-//    ����,�������
+/**************************************************************************/
+/*
+    @brief  IIC写单字节
+
+    @param  reg:寄存器地址
+	@param  data:数据(uint8_t)
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Write_Byte(u8 reg,u8 data) 				 
 { 
   IICStart(&MPU_bus); 
-	IICSendByte(&MPU_bus, (MPU_ADDR<<1)|0);//����������ַ+д����	
-	if(IICWaitAck(&MPU_bus))	//�ȴ�Ӧ��
+	IICSendByte(&MPU_bus, (MPU_ADDR<<1)|0);
+	if(IICWaitAck(&MPU_bus))	
 	{
 		IICStop(&MPU_bus);		 
 		return 1;		
 	}
-	IICSendByte(&MPU_bus,reg);	//д�Ĵ�����ַ
-	IICWaitAck(&MPU_bus);		//�ȴ�Ӧ�� 
-	IICSendByte(&MPU_bus,data);//��������
-	if(IICWaitAck(&MPU_bus))	//�ȴ�ACK
+	IICSendByte(&MPU_bus,reg);	
+	IICWaitAck(&MPU_bus);		
+	IICSendByte(&MPU_bus,data);
+	if(IICWaitAck(&MPU_bus))	
 	{
 		IICStop(&MPU_bus);	 
 		return 1;		 
@@ -242,26 +332,47 @@ u8 MPU_Write_Byte(u8 reg,u8 data)
   IICStop(&MPU_bus);	 
 	return 0;
 }
-//IIC��һ���ֽ� 
-//reg:�Ĵ�����ַ 
-//����ֵ:����������
+
+
+/**************************************************************************/
+/*
+    @brief  IIC读单字节
+
+    @param  reg:寄存器地址
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Read_Byte(u8 reg)
 {
 	u8 res;
   IICStart(&MPU_bus); 
-	IICSendByte(&MPU_bus,(MPU_ADDR<<1)|0);//����������ַ+д����	
-	IICWaitAck(&MPU_bus);		//�ȴ�Ӧ�� 
-  IICSendByte(&MPU_bus,reg);	//д�Ĵ�����ַ
-  IICWaitAck(&MPU_bus);		//�ȴ�Ӧ��
+	IICSendByte(&MPU_bus,(MPU_ADDR<<1)|0);
+	IICWaitAck(&MPU_bus);		
+  IICSendByte(&MPU_bus,reg);	
+  IICWaitAck(&MPU_bus);		
   IICStart(&MPU_bus);
-	IICSendByte(&MPU_bus,(MPU_ADDR<<1)|1);//����������ַ+������	
-  IICWaitAck(&MPU_bus);		//�ȴ�Ӧ�� 
-	res=IICReceiveByte(&MPU_bus);//��ȡ����,����nACK 
+	IICSendByte(&MPU_bus,(MPU_ADDR<<1)|1);
+  IICWaitAck(&MPU_bus);		
+	res=IICReceiveByte(&MPU_bus);
 	IICSendNotAck(&MPU_bus);
-  IICStop(&MPU_bus);			//����һ��ֹͣ���� 
+  IICStop(&MPU_bus);			
 	return res;		
 }
 
+
+/**************************************************************************/
+/*
+    @brief  IIC连续读
+
+    @param  addr:器件地址
+    @param  reg:寄存器地址
+	@param  len:写入长度
+	@param  buf:数据区
+
+    @return 0 if success
+*/
+/**************************************************************************/
 u8 MPU_Read_Len(u8 addr,u8 reg,u8 len,u8 *buf)
 { 
  	IICStart(&MPU_bus); 
@@ -313,6 +424,17 @@ uint8_t MPU_Read_Multi_Byte(uint8_t addr, uint8_t length, uint8_t buff[])
 	return 0;
 }
 
+
+/**************************************************************************/
+/*
+    @brief  get the roll and pitch
+
+    @param  roll:roll(float)
+    @param  pitch:pitch(float)
+
+    @return NULL
+*/
+/**************************************************************************/
 void MPU_Get_Angles(float * roll,float * pitch)
 {
 	short ax,ay,az;
@@ -321,6 +443,16 @@ void MPU_Get_Angles(float * roll,float * pitch)
 	*roll = atanf((float)ay/(float)az);
 }
 
+
+/**************************************************************************/
+/*
+    @brief  check the MPU6050 is horizontal or not
+
+    @param  NULL
+
+    @return 1 if is horizontal
+*/
+/**************************************************************************/
 uint8_t MPU_isHorizontal(void)
 {
 	float roll,pitch;
