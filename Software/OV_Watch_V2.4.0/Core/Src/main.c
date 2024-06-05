debacbb0a9b141ac7601540e56400ef9769ad3f3
kingham <1425962791@qq.com>
2024-06-05 21:15:16 +0800

    修改APP开机启动信息

--- Software/OV_Watch_V2.4.0/Core/Src/main.c
+++ Software/OV_Watch_V2.4.0/Core/Src/main.c
@@ -249,7 +249,7 @@ int main(void)
 	delay_ms(10);
 	LCD_Set_Light(50);
 	LCD_ShowString(72,LCD_H/2,(uint8_t*)"Welcome!",WHITE,BLACK,24,0);//12*6,16*8,24*12,32*16
-	LCD_ShowString(36,LCD_H/2+48,(uint8_t*)"OV-Watch V2.4.0",WHITE,BLACK,24,0);
+	LCD_ShowString(36,LCD_H/2+48,(uint8_t*)"OV-Watch V2.4.1",WHITE,BLACK,24,0);
 	delay_ms(1000);
 	LCD_Fill(0,LCD_H/2-24,LCD_W,LCD_H/2+49,BLACK);
 
